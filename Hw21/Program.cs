55a34534dc0cee41d7463480d5b074122c927822
Dinis <Dinis_abd@example.com>
2022-08-15 14:39:47 +0300

    Hw21

--- Hw21/Program.cs
+++ Hw21/Program.cs
@@ -23,5 +23,5 @@ int y2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату z для второй точки: ");
 int z2 = Convert.ToInt32(Console.ReadLine());
 
-double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y1 - y2) * (y1 - y2) + (z2 - z1) * (z2 - z1));
+double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
 Console.WriteLine("Расстояние между точками равно {0: #.##}", result);
